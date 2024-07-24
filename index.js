a7049182eb0c15eef07277eb1d30464dfefe042b
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2024-07-24 22:57:41 +0500

    file upload and download done

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import cors from 'cors'
 import helmet from 'helmet'
 const app=express()
 import upload from './util/multer.js'
+import os from 'os'
+import fs from 'fs'
+import morgan from 'morgan'
 app.use(express.json())
 app.use(cors())
 app.use(helmet())
-
+app.use(morgan('dev'))
 app.get('/',(req,res)=>{
     try {
         res.send('hello world')
@@ -23,6 +26,20 @@ app.post('/upload',upload.fields([{name:'file'}]),(req,res)=>{
         console.log(error)
     }
 })
+
+app.get('/files', (req, res) => {
+   
+    const path = './uploads/';
+    fs.readdir(path, (err, files) => {
+      res.json(files);
+    });
+  });
+  
+  app.get('/download', (req, res) => {
+    const file = `./uploads/${req.query.filename}`;
+    res.download(file);
+  });
+  
 
 
 app.listen(4000,()=>{
